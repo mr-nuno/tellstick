70ac21b8d7c89068292f2a89f71de0c4e8f9af11
Peter Wikström <peter@wikstrom.in>
2014-10-01 22:59:53 +0200

    Restructuring of base tellstick app

--- index.js
+++ index.js
@@ -1,58 +1,16 @@
-var Hapi = require('hapi');
-var child = require('child_process');
+'use strict'
 
-// Create a server with a host and port
-var server = new Hapi.Server('0.0.0.0', 8000);
+var hapi = require('hapi');
+var routes = require('./app/routes');
 
-// Add the route
-server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function (request, reply) {
-      reply("hello tellstick 2!");
-    }
+//init hapi server
+var server = new hapi.Server('0.0.0.0', 8000);
+
+//routes
+server.route(routes);
+
+//start
+server.start(function() {
+  console.log("server started!")
 });
-
-// Add the route
-server.route({
-    method: 'GET',
-    path: '/device',
-    handler: function (request, reply) {
-      child.exec('/usr/bin/tdtool --list', function (err, stdout, stderr) {
-        reply(stdout);
-      });
-    }
-});
-
-server.route({
-    method: 'PUT',
-    path: '/device/{id}/{action}',
-    handler: function (request, reply) {
-      var id = request.params.id.split(',');
-      var action = request.params.action.split(',');
-      console.log(id + ' ' + action);
-      if (action == 'on') {
-        for(var i = 0; i < id.length; i++){
-          child.exec('/usr/bin/tdtool --on ' + id[i], function () {
-          
-          });
-        }
-
-        reply(true);
-      }
-
-      if (action == 'off') { 
-        for(var i = 0; i < id.length; i++){
-          child.exec('/usr/bin/tdtool --off ' + id[i], function () {
-
-          });
-        }
-
-	reply(true);
-      }
-    }
-});
-
-// Start the server
-server.start();
 
