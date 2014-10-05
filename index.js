d128fe6b0d6bf84e5fdbd82b43ab814b2c13c942
Peter Wikström <peter@wikstrom.in>
2014-10-06 00:10:15 +0200

    Fixed port problem in config, part 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var routes = require('./app/routes');
 var config = require('./app/config');
 
 //init hapi server
-var server = new hapi.Server('0.0.0.0', 80, config.server);
+var server = new hapi.Server('0.0.0.0', process.env.PORT, config.server);
 
 //routes
 server.route(routes);
